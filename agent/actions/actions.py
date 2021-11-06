46ed4ffa0cde5e302aa37a00780b4c40d0db7053
shivambang <shivam.bang@gmail.com>
2021-11-07 01:07:48 +0530

    Improve Bot

--- agent/actions/actions.py
+++ agent/actions/actions.py
@@ -7,9 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 import re, json
+from random import randint
 from . import dbconnect
 from typing import Any, Text, Dict, List
-
+from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -33,22 +34,32 @@ class ActionGetCourses(Action):
         courses = {}
         for course in dbconnect.getCourses(ctype):
             print(course)
+            if ctype == 'cis' and (course[0] == 'CIS4930' or course[0] == 'CIS6930'):
+                continue
             if level == 'grad' and int(re.sub('\D', '', course[0])) > 5000: 
-                courses[course[0]] = course[1]
+                courses[course[0]] = list(course)
             if level == 'undergrad' and int(re.sub('\D', '', course[0])) < 5000: 
-                courses[course[0]] = course[1]
+                courses[course[0]] = list(course)
+            
         m = ""
+        cval = []
+        keys = []
         if courses == {} :
             m = f"no {ctype} courses"
         else:
-            cval = [*courses.values()]
-            m = " ".join(cval)
+            cval = list(courses.values())
+            keys = [*courses.keys()]
+            m = ", ".join([i[1] for i in cval])
             if len(cval) > 3:
-                m = ", ".join(cval[:3])
+                m = ", ".join([i[1] for i in cval[:3]])
                 m += f" and {len(cval) - 3} other {level} {ctype} courses in Spring 2022"
-        message = f"The CISE department is offering {m}"
+        message = f"The CISE department is offering {m} "
+        for c in cval:
+            message += f"=={c[0]} - {c[1]}"
         dispatcher.utter_message(message)
-        dispatcher.utter_message(json_message= json.dumps(courses))
+        # dispatcher.utter_message(json_message= json.dumps(courses))
+        if len(keys) == 1:
+            return [SlotSet("course", keys[0])]
         
 class ActionGetCourseInfo(Action):
 
@@ -67,7 +78,7 @@ class ActionGetCourseInfo(Action):
                 courses[course[0]] = course[2]
             if level == 'undergrad' and int(re.sub('\D', '', course[0])) < 5000: 
                 courses[course[0]] = course[2]
-        message = "Multiple Courses Found!"
+        message = courses
         if len(courses) == 1:
             for k in courses:
                 message = f"{k} {courses[k]}"
@@ -94,7 +105,8 @@ class ActionGetCourseInst(Action):
         if len(courses) == 1:
             for k in courses:
                 message = f"Professor {courses[k]} will be teaching {k} "
-                message = f"{k} will be taught by {courses[k]}"
+                if randint(1, 10) > 5:
+                    message = f"{k} will be taught by {courses[k]}"
         dispatcher.utter_message(message)
         
 class ActionGetCourseText(Action):
@@ -118,7 +130,8 @@ class ActionGetCourseText(Action):
         if len(courses) == 1:
             for k in courses:
                 message = f"There are no required textbooks for {k}"
-                message = f"You wont need any textbooks for {k}"
+                if randint(1, 10) > 5:
+                    message = f"You wont need any textbooks for {k}"
         dispatcher.utter_message(message)
         
 class ActionGetCourseTime(Action):
@@ -134,15 +147,19 @@ class ActionGetCourseTime(Action):
         c = tracker.get_slot('course')
         courses = {}
         for course in dbconnect.getClassTime(c):
-            if level == 'grad' and int(re.sub('\D', '', course[0])) > 5000: 
-                courses[course[0]] = course[2]
-            if level == 'undergrad' and int(re.sub('\D', '', course[0])) < 5000: 
-                courses[course[0]] = course[2]
-        message = "Multiple Courses Found!"
+            if (level == 'grad' and int(re.sub('\D', '', course[0])) > 5000) or (level == 'undergrad' and int(re.sub('\D', '', course[0])) < 5000): 
+                if course[0] in courses:
+                    courses[course[0]].append(list(course))
+                else:
+                    courses[course[0]] = [list(course)]
+
+        message = "Multiple Courses Found!" 
         if len(courses) == 1:
             for k in courses:
-                message = f"{courses[k]} will be taught on {k} "
-                message = f"{k} will be taught by {courses[k]}"
+                c = courses[k][0]
+                message = f"{c[0]} will be taught on {c[4]} from {str(c[2])[:-3]} to {str(c[3])[:-3]} in {c[5]}"
+                # for c in courses[k][1:]:
+                #     message += f" and on {c[4]} from {str(c[2])[:-3]} to {str(c[3])[:-3]} in {c[5]}"
         dispatcher.utter_message(message)
         
 class ActionGetCoursePreq(Action):
@@ -168,3 +185,23 @@ class ActionGetCoursePreq(Action):
                 message = f"{courses[k]} "
         dispatcher.utter_message(message)
         
+class ActionFilterCourseInst(Action):
+
+    def name(self) -> Text:
+        return "action_filter_course_inst"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        level = tracker.get_slot('course_level')
+        c = tracker.get_slot('faculty')
+        courses = {}
+        for course in dbconnect.getCoursesByInst(c):
+            if level == 'grad' and int(re.sub('\D', '', course[0])) > 5000: 
+                courses[course[2]] = course[1]
+            if level == 'undergrad' and int(re.sub('\D', '', course[0])) < 5000: 
+                courses[course[2]] = course[1]
+        message = ", ".join([*courses.values()])
+        message = f"{c} will be teaching {message}"
+        dispatcher.utter_message(message)
