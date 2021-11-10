eb6fd1c574abf6bf9514c772fa83c74a78e2f385
bishnoirajat2 <34034754+bishnoirajat2@users.noreply.github.com>
2021-11-10 12:28:53 -0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -31,5 +31,27 @@ def bot_uttered(message):
 while True:
     r = conn.recv(1024).decode()
     if r != "": sio.emit('user_uttered', {"message": r})
+        
+        
+        
+from socket import *
+
+HOST = "10.192.141.201"
+#local host
+PORT = 6000 #open port 7000
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1) #how many connections can it receive at one time
+conn, addr = s.accept() #accept the connection
+print("Connected by: ", addr) #print the address of the person connected
+while True:
+    data = conn.recv(1024).decode() #how many bytes of data will the server receive
+    print("Received: ",data)
+    reply = input("Reply: ") #server 's reply to the client
+    reply += '\n'    
+    conn.send(reply.encode('utf-8'))
+conn.close()
+
+
     
 
